94c0842abed1ad031965a241c574715a5d676336
Alltch <alltchumb05@gmail.com>
2025-12-03 16:42:19 +0100

    U

--- moving_average.py
+++ moving_average.py
@@ -1,7 +1,17 @@
 import numpy as np
 
 def moving_average(signal, window_size):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if window_size % 2 == 0:
+        raise ValueError("Window size must be odd.")
+    n = len(signal)
+    k = (window_size - 1) // 2
 
+    result = np.zeros(n, dtype=float)
+
+    for i in range(n):
+        left = max(0, i - k)
+        right = min(n, i + k + 1)
+        window = signal[left:right]
+        result[i] = np.mean(window)
+
+    return result
