51b1a58ec29a5f15e662394a4ce874ffa5bba20d
Alltch <alltchumb05@gmail.com>
2025-12-03 16:22:48 +0100

    U

--- count_values_in_bins.py
+++ count_values_in_bins.py
@@ -1,7 +1,26 @@
 import numpy as np
 
 def count_values_in_bins(data, bin_edges):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    B = len(bin_edges) - 1
 
+    counts = [0] * B
+
+    for x in data:
+        if x < bin_edges[0] or x > bin_edges[-1]:
+            continue
+
+        for i in range(B):
+            left = bin_edges[i]
+            right = bin_edges[i + 1]
+
+            if i == B - 1:
+                if left <= x <= right:
+                    counts[i] += 1
+                    break
+
+            else:
+                if left <= x < right:
+                    counts[i] += 1
+                    break
+
+    return np.array(counts, dtype=int)
